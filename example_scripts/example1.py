d004879c7bc51d639b7965ed35cec5ee6f8040fc
anubhav-dogra <anubhavdograa@gmail.com>
2025-12-02 23:45:29 +0000

    linting capabilities

--- example_scripts/example1.py
+++ example_scripts/example1.py
@@ -7,7 +7,5 @@ print("Action space:", env.action_space)
 for i in range(10):
     action = env.action_space.sample()
     obs, reward, terminated, truncated, info = env.step(action)
-    print(
-        f"step {i}: reward={reward}, terminated={terminated}, truncated={truncated}"
-    )
+    print(f"step {i}: reward={reward}, terminated={terminated}, truncated={truncated}")
 env.close()
